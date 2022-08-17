e195f0203efd506f8f36be1a4de45e47435513be
Nyaka <dammnyaka@gmail.com>
2022-08-17 13:52:15 +0300

    remove fix

--- src/component/Main/Main.jsx
+++ src/component/Main/Main.jsx
@@ -3,6 +3,7 @@ import { Line } from "react-chartjs-2";
 import { Chart as ChartJS } from "chart.js/auto";
 import List from "./List";
 import "./Main.scss";
+import removeIcon from "../../icon/remove.png";
 
 const Main = ({ items, hidden, setHidden, options, setItems }) => {
   const handleClick = (item) => {
@@ -10,10 +11,10 @@ const Main = ({ items, hidden, setHidden, options, setItems }) => {
     console.log(item);
   };
 
-  const remove = (item, e) => {
+  const remove = (item, e, index) => {
     e.stopPropagation();
-    if (item.id !== -1) {
-      setItems(items.splice(item.id - 1, 1));
+    if (item.id) {
+      setItems(items.splice(index, 1));
     }
   };
 
@@ -27,10 +28,13 @@ const Main = ({ items, hidden, setHidden, options, setItems }) => {
       </div>
       <div className="table_body">
         {items &&
-          items.map((item) => (
+          items.map((item, index) => (
             <div className="body" key={item.id} onClick={() => handleClick(item)}>
               <div className="body_item">
-                <div>{item.name}</div>
+                <div className="body_name">
+                  <div>{item.name}</div>
+                  {options && <img onClick={(e) => remove(item, e, index)} src={removeIcon} alt="remove" />}
+                </div>
                 <div className="body_item_today">
                   <div>{item.today}</div>
                 </div>
@@ -39,7 +43,6 @@ const Main = ({ items, hidden, setHidden, options, setItems }) => {
                   <div className={item.percent > 0 ? "precent_green" : "precent_red"}>{`${item.percent}%`}</div>
                 </div>
                 <div>{item.weeks}</div>
-                {options && <input className="remove" type="checkbox" onClick={(e) => remove(item, e)} />}
               </div>
               {hidden === item.id && (
                 <div className="body_list" onClick={(e) => e.stopPropagation()}>
